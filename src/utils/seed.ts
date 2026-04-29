14e29266fdd260e942047f5ba4d90a75fde0edc9
Tyler Neuenschwander <tcneuen@gmail.com>
2026-04-29 15:17:30 -0500

    use items batch

--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -2,14 +2,12 @@ import { faker } from "@faker-js/faker";
 import { apiFetch } from "./api";
 
 export async function seedList(collectionId: string, listId: string) {
-  for (let i = 0; i < 10; i++) {
-    await apiFetch(`/collections/${collectionId}/items`, {
-      method: "POST",
-      body: JSON.stringify({
-        name: faker.commerce.productName(),
-        description: faker.commerce.productDescription(),
-        listId,
-      }),
-    });
-  }
+  const items = Array.from({ length: 10 }, () => ({
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+  }));
+  await apiFetch(`/collections/${collectionId}/items/batch`, {
+    method: "POST",
+    body: JSON.stringify({ listId, items }),
+  });
 }
